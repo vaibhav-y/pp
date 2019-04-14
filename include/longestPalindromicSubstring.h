68c60eb17423c2886059c684e95474b27aa9331b
vaibhav-y <yvvaibhav@gmail.com>
2019-04-14 15:30:30 -0400

    P005: Done

--- include/longestPalindromicSubstring.h
+++ include/longestPalindromicSubstring.h
@@ -12,12 +12,13 @@ static_assert(false, "Won't include multiple solution headers together!")
 namespace p005 {
 class Solution {
 private:
-  inline std::string grow(std::string src, size_t begin, size_t end) {
-    while (begin >= 0 && end < src.size() && src[begin] == src[end]) {
-      --begin;
+  inline std::string grow(const std::string &src, size_t begin, size_t end) {
+    while (begin >= 1 && end + 1 < src.size() &&
+           src[begin - 1] == src[end + 1]) {
       ++end;
+      --begin;
     }
-    return src.substr(begin + 1, end - begin + 1);
+    return src.substr(begin, end - begin + 1);
   }
 
 public:
@@ -27,11 +28,14 @@ public:
     }
 
     std::string longest{s[0]};
-    for (size_t c = 0; c < s.size(); ++c) {
+    for (size_t c = 0; c < s.size() - 1; ++c) {
       std::string temp{grow(s, c, c)};
       longest = temp.size() > longest.size() ? temp : longest;
-      temp = grow(s, c, c + 1);
-      longest = temp.size() > longest.size() ? temp : longest;
+      // Only grow even length ones if we found a palindromic center:
+      if (s[c] == s[c + 1]) {
+        temp = grow(s, c, c + 1);
+        longest = temp.size() > longest.size() ? temp : longest;
+      }
     }
 
     return longest;
