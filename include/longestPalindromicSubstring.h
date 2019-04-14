e6a5fc8f3a2e7a5e3fa14c7e901ca04dae8e02b7
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-04-13 22:06:47 -0400

    P005: Attempt 2

--- include/longestPalindromicSubstring.h
+++ include/longestPalindromicSubstring.h
@@ -3,6 +3,7 @@ static_assert(false, "Won't include multiple solution headers together!")
 #else
 #define INCLUDED_SOLUTION_HEADER "005"
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -11,71 +12,37 @@ static_assert(false, "Won't include multiple solution headers together!")
 namespace p005 {
 class Solution {
 private:
-  using position_t = std::pair<int, int>;
-
-  bool isPalindrome(std::string &s) {
-    bool palindromic = true;
+  inline bool isPalindrome(const std::string &s) const {
     auto cit = s.cbegin();
     auto crit = s.crbegin();
-    for (; cit != s.cend() && crit != s.crend(); ++cit, ++crit) {
-      palindromic &= ((*cit) == (*crit));
-    }
-    return palindromic;
-  }
 
-  // Compute the manhattan distance
-  inline size_t mdist(position_t start, position_t end) {
-    return abs(start.first - end.first) + abs(start.second - end.second);
+    while (*crit == *cit && cit != s.cend()) {
+      ++cit;
+      ++crit;
+    }
+
+    return cit == s.cend();
   }
 
 public:
   std::string longestPalindrome(std::string s) {
     size_t length = s.size();
-    if (length < 2 || isPalindrome(s)) {
+    if (length < 2) {
       return s;
     }
 
-    std::vector<std::vector<short>> in_palindrome{
-        length, std::vector<short>(length, 0)};
-
-    for (size_t i = 0, len = length; i < length; ++i) {
-      for (size_t j = 0; j < length; ++j) {
-        if (s[len - 1 - i] == s[j]) {
-          in_palindrome[i][j] = 1;
+    // Scan string for palindromes of various lengths
+    for (size_t len = length; len != 0; --len) {
+      for (size_t start = 0; start + len - 1 < length; ++start) {
+        auto substring = s.substr(start, len);
+        if (isPalindrome(substring)) {
+          return substring;
         }
       }
     }
 
-    std::pair<size_t, size_t> best_start{0, 0};
-    std::pair<size_t, size_t> best_end{0, 0};
-
-    // Only look for length 2 and above since we find length 1 by default
-    for (size_t i = 0; i < length - 1; ++i) {
-      for (size_t j = 0; j < length - 1; ++j) {
-        // Start counting down right
-        if (in_palindrome[i][j]) {
-          std::pair<size_t, size_t> end{i, j};
-          while (end.first < length - 1 && end.second < length - 1 &&
-                 in_palindrome[end.first + 1][end.second + 1]) {
-            ++end.first;
-            ++end.second;
-            // Never visit this chain again
-            in_palindrome[end.first][end.second] = 0;
-          }
-          if (mdist(best_start, best_end) < mdist({i, j}, end)) {
-            best_start = {i, j};
-            best_end = end;
-          }
-        }
-      }
-    }
-
-    // Match occurred in lower triangular part
-    if (best_start.second < best_end.second) {
-      return s.substr(best_start.second,
-                      best_end.second - best_start.second + 1);
-    }
-    return s.substr(best_start.first, best_end.first - best_start.first + 1);
+    // UNREACHABLE
+    return {};
   }
 };
 } // namespace p005
