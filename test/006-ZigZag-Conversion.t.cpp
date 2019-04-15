5f7a787a31613ef569367632fa4da595c4d7d515
vaibhav-y <yvvaibhav@gmail.com>
2019-04-15 00:14:40 -0400

    p-006: Initial implementation along with notes

---
+++ test/006-ZigZag-Conversion.t.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+#include "zigzagConversion.h"
+
+class P006_ZigZagConversion : public ::testing::Test {
+protected:
+  p006::Solution sol;
+};
+
+TEST_F(P006_ZigZagConversion, EmptyStr) {
+  EXPECT_EQ(sol.convert("", 1), "");
+  EXPECT_EQ(sol.convert("", 2), "");
+  EXPECT_EQ(sol.convert("", 3), "");
+  EXPECT_EQ(sol.convert("", 4), "");
+}
+
+TEST_F(P006_ZigZagConversion, StrSize1) {
+  EXPECT_EQ(sol.convert("A", 1), "A");
+  EXPECT_EQ(sol.convert("B", 2), "B");
+  EXPECT_EQ(sol.convert("C", 3), "C");
+  EXPECT_EQ(sol.convert("D", 4), "D");
+}
+TEST_F(P006_ZigZagConversion, StrSize2) {
+  EXPECT_EQ(sol.convert("AB", 1), "AB");
+  EXPECT_EQ(sol.convert("AB", 2), "AB");
+  EXPECT_EQ(sol.convert("AB", 3), "AB");
+  EXPECT_EQ(sol.convert("AB", 4), "AB");
+}
+
+TEST_F(P006_ZigZagConversion, Generic0) {
+  EXPECT_EQ(sol.convert("SOMESTRINGDUNNO", 1), "SOMESTRINGDUNNO");
+}
+
+TEST_F(P006_ZigZagConversion, Generic1) {
+  EXPECT_EQ(sol.convert("ABABABABABABABA", 2), "AAAAAAABBBBBBBA");
+}
+
+TEST_F(P006_ZigZagConversion, LeetCode1) {
+  EXPECT_EQ(sol.convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR");
+}
+
+TEST_F(P006_ZigZagConversion, LeetCode2) {
+  EXPECT_EQ(sol.convert("PAYPALISHIRING", 4), "PINALSIGYAHRPI");
+}
