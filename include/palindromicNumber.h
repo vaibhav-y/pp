ea4b78d5090b29cc1b881d0e43882180ce274912
vaibhav-y <yvvaibhav@gmail.com>
2019-04-21 22:57:02 -0400

    p-009: Complete submission

---
+++ include/palindromicNumber.h
@@ -0,0 +1,27 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multiple solution headers");
+#else
+
+namespace p009 {
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if (x == 0) {
+      return true;
+    }
+    else if (x < 0 || x % 10 == 0) {
+      return false;
+    }
+    int reversed = 0;
+
+    while (x > reversed) {
+      reversed = 10 * reversed + x % 10;
+      x /= 10;
+    }
+
+    return (reversed == x) || (reversed / 10 == x);
+  }
+};
+}
+
+#endif
