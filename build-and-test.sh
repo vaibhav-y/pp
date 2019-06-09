44a408b303996ca887b59a32f64ece3276630452
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-06-09 15:23:45 -0400

    Rely on env for CC, CXX

--- build-and-test.sh
+++ build-and-test.sh
@@ -5,11 +5,8 @@
     && mkdir ./build \
     && echo "Done."
 
-(
-    CC=/usr/local/bin/clang \
-    CXX=/usr/local/bin/clang++ \
-    cmake -G Ninja \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -Bbuild -H. && \
-    VERBOSE=1 ninja -C build && \
-    CTEST_OUTPUT_ON_FAILURE=1 GTEST_COLOR=1 ninja -v -C build test)
+cmake -G Ninja \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -Bbuild -H. && \
+VERBOSE=1 ninja -C build && \
+CTEST_OUTPUT_ON_FAILURE=1 GTEST_COLOR=1 ninja -v -C build test
