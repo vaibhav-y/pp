871cffb1934b02e1b250d8034e5b4ddc34e04553
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-06-09 15:12:00 -0400

    p-011: Start

---
+++ include/maxArea2D.h
@@ -0,0 +1,17 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multiple solution headers");
+#else
+
+#include <vector>
+
+namespace p011 {
+class Solution {
+public:
+  int maxArea(std::vector<int> &height) {
+    int maximum = -1;
+    return maximum;
+  }
+};
+}
+
+#endif
---
+++ test/011-Container-With-Most-Water.t.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <utility>
+
+#include "maxArea2D.h"
+
+class P011_ContainerWithMostWater :
+  public ::testing::TestWithParam<std::pair<std::vector<int>, int>> {
+protected:
+  p011::Solution sol;
+};
+
+INSTANTIATE_TEST_CASE_P(MaxAreaTest, P011_ContainerWithMostWater,
+  ::testing::Values(
+    std::make_pair<std::vector<int>, int>({1, 1}, 1)
+  ));
+
+
+TEST_P(P011_ContainerWithMostWater, Parameterized) {
+  // FIXME Not a good idea to do this, but I can't figure this out
+  // I am screamed at for discarding a seemingly non-existent const qualifier
+  // I know map<K,V> is has an implied <const K, V> pair internally
+  // But why is a list of pairs showing this behaviour... ?!
+  // Thus we cast away the qualifier we never added.
+  int area = sol.maxArea(const_cast<std::vector<int>&>(GetParam().first));
+  EXPECT_EQ(area,  GetParam().second);
+}
