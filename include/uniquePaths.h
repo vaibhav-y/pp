c4a54e30025a025eb55daa018aeb9cdf77216d84
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-06-24 00:33:25 -0400

    Start 062

---
+++ include/uniquePaths.h
@@ -0,0 +1,12 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multiple solution headers");
+#endif
+
+#define INCLUDED_SOLUTION_HEADER
+
+namespace p062 {
+class Solution {
+public:
+  int uniquePaths(int m, int n) {}
+};
+} // namespace p062
