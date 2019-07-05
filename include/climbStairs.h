1df5597bdfa8bea2634079467a979c4cbad824fa
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-07-04 21:15:10 -0400

    Complete leetcode 070

---
+++ include/climbStairs.h
@@ -0,0 +1,28 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multiple solution headers");
+#endif
+
+#define INCLUDED_SOLUTION_HEADER
+
+namespace p070 {
+class Solution {
+public:
+  int climbStairs(int n) {
+    if (n < 2) {
+      return 1;
+    }
+    if (n == 2) {
+      return 2;
+    }
+    int current = 2;
+    int back_1 = 1;
+    while (n > 2) {
+      int temp = current + back_1;
+      back_1 = current;
+      current = temp;
+      --n;
+    }
+    return current;
+  }
+};
+} // namespace p070
