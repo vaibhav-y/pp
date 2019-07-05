e5d8a52abb4a233525068f0fe2a98a32fc61ab62
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-07-04 21:15:10 -0400

    Complete leetcode 070

---
+++ test/070-Climbing-Stairs.t.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <utility>
+
+#include "climbStairs.h"
+
+typedef int INPUT;
+typedef int OUTPUT;
+typedef std::pair<INPUT, OUTPUT> TESTCASE;
+
+auto& t = std::make_pair<INPUT, OUTPUT>;
+
+class P070_ClimbingStairs : public ::testing::TestWithParam<TESTCASE> {
+  protected:
+  p070::Solution sol;
+};
+
+INSTANTIATE_TEST_CASE_P(ClimbingStairsTest, P070_ClimbingStairs,
+  ::testing::Values(
+    t(1, 1),
+    t(2, 2),
+    t(3, 3),
+    t(4, 5),
+    t(5, 8),
+    t(6, 13),
+    t(10, 89),
+    t(45, 1836311903)
+  ));
+
+
+TEST_P(P070_ClimbingStairs, Generic) {
+  EXPECT_EQ(GetParam().second,
+            sol.climbStairs(GetParam().first));
+}
