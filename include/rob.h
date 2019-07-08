c73c0f620a08454ec9b8572d50ae79d2f9993d2d
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-07-07 22:55:57 -0400

    Add tests for 198

---
+++ include/rob.h
@@ -0,0 +1,14 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multuple solution headers");
+#endif
+
+#define INCLUDED_SOLUTION_HEADER
+
+#include <vector>
+
+namespace p0198 {
+class Solution {
+public:
+  int rob(std::vector<int> &nums) {}
+};
+} // namespace p0198
---
+++ src/0198-House-Robber.cpp
@@ -0,0 +1,1 @@
+#include "rob.h"
