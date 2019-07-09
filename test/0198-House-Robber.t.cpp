eb39a48206a4ea5b79e597534ed30254fd75fe2a
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-07-08 20:36:19 -0400

    Complete 198

--- test/0198-House-Robber.t.cpp
+++ test/0198-House-Robber.t.cpp
@@ -2,9 +2,13 @@
 #include <utility>
 #include <vector>
 
+#include "rob.h"
+
 typedef std::vector<int> INPUT;
 typedef int OUTPUT;
 typedef std::pair<INPUT, OUTPUT> TESTCASE;
+
+auto &t = std::make_pair<INPUT, OUTPUT>;
 
 class P0198_HouseRobber : public ::testing::TestWithParam<TESTCASE> {
 protected:
@@ -13,12 +17,12 @@ protected:
 
 INSTANTIATE_TEST_CASE_P(HouseRobberTest, P0198_HouseRobber,
   ::testing::Values(
-    {{1}, 1},
-    {{1, 2}, 2},
-    {{2, 1}, 2},
-    {{1, 2, 3, 1}, 4},
-    {{2, 7, 9, 3, 1}, 12},
-    {{2, 1, 4, 3, 5, 1000, 7, 8, 6}, 1014}
+    t({1}, 1),
+    t({1, 2}, 2),
+    t({2, 1}, 2),
+    t({1, 2, 3, 1}, 4),
+    t({2, 7, 9, 3, 1}, 12),
+    t({2, 1, 4, 3, 5, 1000, 7, 8, 6}, 1014)
   )
 );
 
