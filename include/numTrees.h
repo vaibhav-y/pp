144f671aff376d928b29e05ef9f684a2bdc34640
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-09 21:12:48 -0400

    Complete 96

--- include/numTrees.h
+++ include/numTrees.h
@@ -4,11 +4,31 @@ static_assert(false, "Won't include multiple solution headers");
 
 #define INCLUDED_SOLUTION_HEADEAR
 
+#include <vector>
+#include <numeric>
+
 namespace p0096 {
 class Solution {
 public:
   int numTrees(int n) {
+    if (n < 2) {
+      return 1;
+    }
+    std::vector<int> count(static_cast<size_t>(n + 1));
+    // Base cases
+    count[0] = 1;
+    count[1] = 1;
 
+    // Iteratively compute the sums
+    // Each sum is the dot product of [1..k] with [k..1]
+    for (size_t i = 2; i <= n; ++i) {
+      count[i] = 0;
+      for (size_t j = 0; j < i; ++j) {
+        count[i] += count[j] * count[i -  1 - j];
+      }
+    }
+
+    return count[n];
   }
 };
 } // namespace p0096
--- test/0096-unique-binary-search-trees.t.cpp
+++ test/0096-unique-binary-search-trees.t.cpp
@@ -18,13 +18,22 @@ INSTANTIATE_TEST_CASE_P(NumTreesTest, P0096_NumTress,
   ::testing::Values(
     t(0, 1),
     t(1, 1),
-    t(2, 2)
+    t(2, 2),
+    t(3, 5),
+    t(4, 14),
+    t(5, 42),
+    t(6, 132),
+    t(7, 429),
+    t(8, 1430),
+    t(9, 4862),
+    // This is the largest number
+    t(19, 1767263190)
   )
 );
 
 TEST_P(P0096_NumTress, Generic) {
   EXPECT_EQ(
-    GetParam().first,
-    sol.numTrees(GetParam().second)
+    GetParam().second,
+    sol.numTrees(GetParam().first)
   );
 }
