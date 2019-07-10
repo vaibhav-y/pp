2ca89dbc7db0cda5ee0dbd5084852ae46a52aff2
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-10 17:21:57 -0400

    Add sample code

--- include/minimumTotal.h
+++ include/minimumTotal.h
@@ -10,7 +10,18 @@ namespace p0120 {
 class Solution {
 public:
   int minimumTotal(std::vector<std::vector<int>> &triangle) {
-    return 0;
+    if (triangle.empty()) {
+      return 0;
+    }
+    if (triangle.size() == 1) {
+      return triangle[0][0];
+    }
+    // This is the most amount of space we'll end up needing,
+    // which should be the same as triangle.size() but lets be explicit about
+    // what this number means
+    const size_t MAX_COLS = triangle.back().size();
+    std::vector<int> row(MAX_COLS);
+    return MAX_COLS;
   }
 };
 }
