7bc715baca2c49cc5cf422e6a3f67628b0f4ce86
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-21 17:20:36 -0400

    334. Increasing Triplet Subsequence
    Completed

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [11. Container With Most Water](https://leetcode.com/problems/container-with-most-water): Re-do, better understand how the greedy choice optimizes the DP relation you came up with.
 * [53. Maximum Subarray](https://leetcode.com/problems/maximum-subarray): I don't know why, but I never get _why_ the solution works.
 * [152. Maximum Product Subarray](https://leetcode.com/problems/maximum-product-subarray/): SAME AS 53. I THINK WE PARTITION OUR SEARCH AROUND THE `0`s. NOT SURE THO TBH.
+* [334. Increasing Triplet Subsequence](https://leetcode.com/problems/increasing-triplet-subsequence): Improve notes.
--- include/increasingTriplet.h
+++ include/increasingTriplet.h
@@ -5,10 +5,29 @@ static_assert(false, "Won't include multiple solution headers");
 #define INCLUDED_SOLUTION_HEADER
 
 #include <vector>
+#include <limits>
 
 namespace p0334 {
 class Solution {
 public:
-  bool increasingTriplet(std::vector<int> &nums) { return false; }
+  bool increasingTriplet(std::vector<int> &nums) {
+    if (nums.size() < 3) {
+      return false;
+    }
+
+    int first = std::numeric_limits<int>::max();
+    int second = std::numeric_limits<int>::max();
+
+    for (auto n : nums) {
+      if (n <= first) {
+        first = n;
+      } else if (n <= second) {
+        second = n;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 } // namespace p0334
