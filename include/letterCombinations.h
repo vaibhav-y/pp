cff6b20cf79a7c977c5f9a9b2e1078c3b8f95f0e
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-28 00:46:57 -0400

    17. Letter Combinations of a Phone Number
    Completed, fastest numbers in the west

---
+++ include/letterCombinations.h
@@ -0,0 +1,41 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+static_assert(false, "Won't include multiple solution headers");
+#endif
+
+#include <vector>
+#include <string>
+
+namespace p0017 {
+class Solution {
+public:
+  std::vector<std::string> letterCombinations(std::string digits) {
+    const std::vector<std::string> letters{"abc", "def", "ghi", "jkl",
+                                           "mno", "pqrs", "tuv", "wxyz"};
+
+    std::vector<std::string> combinations;
+
+    for (auto &c: digits) {
+      int digit = c - '2';
+      if (digit < 0) {
+        return {};
+      }
+      std::vector<std::string> temp;
+      if (combinations.empty()) {
+        for (auto &l : letters[digit]) {
+          temp.push_back(std::string{l});
+        }
+      } else {
+        // Computes the cartesian product of the current array
+        // with all the letters from the 'letters' vector
+        for (auto &l : letters[digit]) {
+          for (auto &str : combinations) {
+            temp.push_back(str + l);
+          }
+        }
+      }
+      std::swap(combinations, temp);
+    }
+    return combinations;
+  }
+};
+}
