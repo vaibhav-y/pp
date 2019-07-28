21a5a685d30e0a7427ac1ed13ca3d5a35fa0d6ea
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-28 01:23:50 -0400

    Apply clang-format

--- include/countPalindromicSubstrings.h
+++ include/countPalindromicSubstrings.h
@@ -4,14 +4,15 @@ static_assert(false, "Won't include multiple solution headers");
 
 #define INCLUDED_SOLUTION_HEADER
 
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 
 namespace p0647 {
 class Solution {
 private:
-  int countPalindromesCenteredAt(const std::string &str, size_t first, size_t last) {
+  int countPalindromesCenteredAt(const std::string &str, size_t first,
+                                 size_t last) {
     int count = 0;
     while (first < last && str[first] == str[last]) {
       ++count;
@@ -22,15 +23,16 @@ private:
     }
     return count;
   }
+
 public:
   int countSubstrings(std::string s) {
     if (s.size() < 2) {
       return s.size();
     }
-    // Trivial case: There are as many size-1 palindromes as there are characters
-    // in the string
+    // Trivial case: There are as many size-1 palindromes as there are
+    // characters in the string
     size_t count = s.size();
-    for (size_t i = 1; i != s.size() ; ++i) {
+    for (size_t i = 1; i != s.size(); ++i) {
       if (i + 1 != s.size() && s[i - 1] == s[i + 1]) {
         count += countPalindromesCenteredAt(s, i - 1, i + 1);
       }
@@ -42,4 +44,4 @@ public:
     return count;
   }
 };
-}
+} // namespace p0647
