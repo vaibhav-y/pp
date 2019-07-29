5c4da728016ceae49ef7558da94558791c12b005
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-28 23:30:02 -0400

    49. Group Anagrams
    Completed

---
+++ include/groupAnagrams.h
@@ -0,0 +1,39 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+#error Won't include multiple solution headers
+#endif
+
+#define INCLUDED_SOLUTION_HEADER
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+namespace p0049 {
+class Solution {
+public:
+  std::vector<std::vector<std::string>>
+  groupAnagrams(std::vector<std::string>& strs) {
+    if (strs.empty()) {
+      return {};
+    }
+    std::unordered_map<std::string, std::vector<std::string>> groupings;
+
+    for (auto &str : strs) {
+      auto key(str);
+      std::sort(key.begin(), key.end());
+      auto it = groupings.find(key);
+      if (it != groupings.end()) {
+        it->second.push_back(str);
+      } else {
+        groupings[key] = {str};
+      }
+    }
+    std::vector<std::vector<std::string>> results;
+    for (auto &group : groupings) {
+      results.push_back(group.second);
+    }
+
+    return results;
+  }
+};
+} // namespace p0049
