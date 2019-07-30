28c831651614a4c53de331492e6a0a9058bcc6d6
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-07-30 01:13:18 -0400

    93. Restore IP Addresses
    Add framework

---
+++ include/restoreIpAddresses.h
@@ -0,0 +1,17 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+#error Won't include multiple solution headers
+#endif
+
+#define INCLUDED_SOLUTION_HEADER
+
+#include <string>
+#include <vector>
+
+namespace p0093 {
+class Solution {
+public:
+  std::vector<std::string> restoreIpAddresses(std::string ip_string) {
+    return {};
+  }
+};
+} // namespace p0093
