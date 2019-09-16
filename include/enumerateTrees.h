8199a12d13014d1613c0586b2804064d1f3a3665
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-09-16 07:04:26 -0400

    95. Unique Binary Search Trees II

---
+++ include/enumerateTrees.h
@@ -0,0 +1,16 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+#error Wont include multiple solution headers
+#endif
+
+#include <vector>
+
+#include <TreeNode.h>
+
+namespace p0095 {
+class Solution {
+public:
+  std::vector<TreeNode*> generateTrees(int n) {
+    
+  }
+};
+} // namespace p0095
