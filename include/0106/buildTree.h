82407e221dcf94d0c942a9a48e2cf1e707b8e7bb
Vaibhav Yenamandra <vyenamandra@bloomberg.net>
2019-09-24 22:23:49 -0400

    106. Construct Binary Tree from Inorder and Postorder Traversal
    Start

---
+++ include/0106/buildTree.h
@@ -0,0 +1,16 @@
+#if defined(INCLUDED_SOLUTION_HEADER)
+#error Wont include multiple solution headers
+#endif
+
+#include <vector>
+
+#include "TreeNode.h"
+
+namespace p0106 {
+class Solution {
+public:
+  TreeNode *buildTree(std::vector<int> &inorder, std::vector<int> &postorder) {
+    return nullptr;
+  }
+};
+}
