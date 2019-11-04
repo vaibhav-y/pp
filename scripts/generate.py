e18aeaed6e7a5189c0d82c94faa11268f113ce12
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2019-11-03 21:46:13 -0500

    Improve boilerplate script with interactivity

--- scripts/generate.py
+++ scripts/generate.py
@@ -7,6 +7,8 @@ import os
 import sys
 import json
 import string
+import subprocess
+from functools import lru_cache
 import requests
 import click
 
@@ -62,6 +64,46 @@ TEST_P(P$number$_$testName, Generic) {
 )
 
 
+def black(text):
+    """Add terminal escape codes for 'black' FG colored text"""
+    return f'\033[30m{text}\033[0m'
+
+
+def red(text):
+    """Add terminal escape codes for 'red' FG colored text"""
+    return f'\033[31m{text}\033[0m'
+
+
+def green(text):
+    """Add terminal escape codes for 'green' FG colored text"""
+    return f'\033[32m{text}\033[0m'
+
+
+def yellow(text):
+    """Add terminal escape codes for 'yellow' FG colored text"""
+    return f'\033[33m{text}\033[0m'
+
+
+def blue(text):
+    """Add terminal escape codes for 'blue' FG colored text"""
+    return f'\033[34m{text}\033[0m'
+
+
+def magenta(text):
+    """Add terminal escape codes for 'magenta' FG colored text"""
+    return f'\033[35m{text}\033[0m'
+
+
+def cyan(text):
+    """Add terminal escape codes for 'cyan' FG colored text"""
+    return f'\033[36m{text}\033[0m'
+
+
+def gray(text):
+    """Add terminal escape codes for 'gray' FG colored text"""
+    return f'\033[90m{text}\033[0m'
+
+
 class QuestionData:
     """Encapsulate the information necessary to fill out our template"""
 
@@ -99,43 +141,56 @@ class QuestionData:
         return includes
 
     @property
+    def index(self):
+        """Question number"""
+        return self._number
+
+    @property
+    @lru_cache(maxsize=1)
     def number(self):
         """Zero padded question number"""
         return f"{self._number:04d}"
 
     @property
+    @lru_cache(maxsize=1)
     def srcFileName(self):
         """Source file path relative to src directory root"""
         return f"{self.number}-{self.titleSlug}.{self.lang}"
 
     @property
+    @lru_cache(maxsize=1)
     def testFileName(self):
         """Test file path relative to test directory root"""
         return f"{self.number}-{self.titleSlug}.t.{self.lang}"
 
     @property
+    @lru_cache(maxsize=1)
     def headerName(self):
         """Base name of the header file"""
         return self.metadata["name"]
 
     @property
+    @lru_cache(maxsize=1)
     def cppIncludeBlock(self):
         """The cpp include block for this question"""
         includes = self.inferredCppIncludes()
         return "\n".join(f"#include <{inc}>" for inc in includes)
 
     @property
+    @lru_cache(maxsize=1)
     def testPrefix(self):
         """Returns the name to use for this test case"""
         # This is in camel case, convert to pascal case
-        name =  self.metadata["name"]
+        name = self.metadata["name"]
         return name[0].capitalize() + name[1:]
 
     @property
+    @lru_cache(maxsize=1)
     def testIdentifier(self):
         """Returns the test case name to use for this test case"""
         # How very original
         return f"{self.testPrefix}Test"
+
 
 class LeetCodeClient:
     """
@@ -156,6 +211,7 @@ class LeetCodeClient:
         self.lang = lang
         self.client = requests.session()
         self.client.get(self.BASE_URL)
+        click.echo(cyan("Session established"))
         if "csrftoken" not in self.client.cookies:
             raise RuntimeError("Couldn't acquire CSRF token for client!")
         self.HEADERS["X-CSRF-Token"] = self.client.cookies["csrftoken"]
@@ -211,14 +267,14 @@ class LeetCodeClient:
                 "variables": {"titleSlug": title},
             }
         )
-
+        click.echo(cyan(f"Send query for: {title}"))
         response = self.client.post(self.ENDPOINT, headers=self.HEADERS, data=body)
 
         if response:
             questionData = response.json().get("data").get("question")
             if not questionData:
-                raise RuntimeError("Malformed response:\n{response.json()}")
-
+                raise RuntimeError("Malformed response:\n{response.text}")
+            click.echo(green("Success! Response decoded"))
             return self.createQuestionData(questionData)
         raise RuntimeError("Query to /graphql failed with:\n{response.text}")
 
@@ -275,14 +331,28 @@ def main(lang, title, srcDir, testDir, includeDir):
         _="_"
     )
     incPath = os.path.join(includeDir, questionData.number)
-    os.mkdir(incPath)
+    os.makedirs(incPath, exist_ok=True)
 
-    with open(os.path.join(incPath, f"{questionData.headerName}.h"), "w") as i:
+    incName = os.path.join(incPath, f"{questionData.headerName}.h")
+    srcName = os.path.join(srcDir, questionData.srcFileName)
+    tstName = os.path.join(testDir, questionData.testFileName)
+
+    with open(incName, "w") as i:
         i.write(headerContents)
-    with open(os.path.join(srcDir, questionData.srcFileName), "w") as s:
+        click.echo(green("Wrote: " + incName))
+    with open(srcName, "w") as s:
         s.write(srcContents)
-    with open(os.path.join(testDir, questionData.testFileName), "w") as t:
+        click.echo(green("Wrote: " + srcName))
+    with open(tstName, "w") as t:
         t.write(testContents)
+        click.echo(green("Wrote: " + t.name))
+
+    branch = f"p-{questionData.index}"
+    if click.confirm(yellow(f"Checkout branch: '{branch}'?"), default=True):
+        p = subprocess.run(["git", "checkout", "-b", branch], check=True,
+                capture_output=True, encoding="utf-8")
+        click.echo(cyan(p.stderr))
+
     return 0
 
 if __name__ == "__main__":
